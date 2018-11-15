1f7c738033fc522b48c6d2931b5a9c071f1998ac
Vchabs <vchabs@gmail.com>
2018-11-14 19:07:02 -0500

    updated heroku settings

--- dev/settings/heroku.py
+++ dev/settings/heroku.py
@@ -8,23 +8,23 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 HTTPS='on'
 ACCOUNT_DEFAULT_HTTP_PROTOCOL='https'
 
-CACHES = {
-    'default': {
-        'BACKEND': 'redis_cache.RedisCache',
-        'LOCATION': [os.environ['REDIS_SERVER'] + ':6379'],
-        'OPTIONS': {
-            'DB': 0,
-            'PARSER_CLASS': 'redis.connection.HiredisParser',
-            'CONNECTION_POOL_CLASS': 'redis.BlockingConnectionPool',
-            'CONNECTION_POOL_CLASS_KWARGS': {
-                'max_connections': 200,
-                'timeout': 20,
-            },
-            'MAX_CONNECTIONS': 1000,
-            'PICKLE_VERSION': -1,
-        },
-    },
-}
+# CACHES = {
+#     'default': {
+#         'BACKEND': 'redis_cache.RedisCache',
+#         'LOCATION': [os.environ['REDIS_SERVER'] + ':6379'],
+#         'OPTIONS': {
+#             'DB': 0,
+#             'PARSER_CLASS': 'redis.connection.HiredisParser',
+#             'CONNECTION_POOL_CLASS': 'redis.BlockingConnectionPool',
+#             'CONNECTION_POOL_CLASS_KWARGS': {
+#                 'max_connections': 200,
+#                 'timeout': 20,
+#             },
+#             'MAX_CONNECTIONS': 1000,
+#             'PICKLE_VERSION': -1,
+#         },
+#     },
+# }
 
 # DATABASES = {
 #         'default': {
