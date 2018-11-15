19a3ef29f9daef2367a609f94fa7fd362e4f7309
Vchabs <vchabs@gmail.com>
2018-11-14 19:03:19 -0500

    updated settings

--- dev/settings/heroku.py
+++ dev/settings/heroku.py
@@ -26,20 +26,21 @@ CACHES = {
     },
 }
 
-DATABASES = {
-        'default': {
-            'ENGINE': os.environ['DATABASE_ENGINE'],
-            'NAME': os.environ['DATABASE_NAME'], 
-            'USER': os.environ['DATABASE_USER'],
-            'PASSWORD': os.environ['DATABASE_PASSWORD'], 
-            'HOST':os.environ['DATABASE_HOST'],    
-            'PORT': os.environ['DATABASE_PORT'] 
-        }
-    }
-CONN_MAX_AGE=30
+# DATABASES = {
+#         'default': {
+#             'ENGINE': os.environ['DATABASE_ENGINE'],
+#             'NAME': os.environ['DATABASE_NAME'], 
+#             'USER': os.environ['DATABASE_USER'],
+#             'PASSWORD': os.environ['DATABASE_PASSWORD'], 
+#             'HOST':os.environ['DATABASE_HOST'],    
+#             'PORT': os.environ['DATABASE_PORT'] 
+#         }
+#     }
+# CONN_MAX_AGE=30
 
 django_heroku.settings(locals())
-
+import dj_database_url
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 
 
